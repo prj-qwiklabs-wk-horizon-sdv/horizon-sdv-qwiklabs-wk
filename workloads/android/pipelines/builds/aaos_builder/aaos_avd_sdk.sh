4e1428dbaeea328de57657b149dde52d0bb5b802
David Smith <dave.m.smith@accenture.com>
2025-02-04 12:19:18 +0000

    TAA-252: Update script documentation

--- workloads/android/pipelines/builds/aaos_builder/aaos_avd_sdk.sh
+++ workloads/android/pipelines/builds/aaos_builder/aaos_avd_sdk.sh
@@ -29,6 +29,16 @@
 #  4. Creates the SDK Addons file.
 #  5. Adds SDK addons to archives for upload.
 #
+# The following variables must be set before running this script:
+#  - AAOS_LUNCH_TARGET: the target device.
+#  - ANDROID_VERSION: the Android version (default: 14).
+#        Determines the API level.
+#
+# Example usage:
+# AAOS_LUNCH_TARGET=sdk_car_x86_64-ap1a-userdebug \
+# ANDROID_VERSION=14 \
+# ./workloads/android/pipelines/builds/aaos_builder/aaos_avd_sdk.sh
+
 # Include common functions and variables.
 # shellcheck disable=SC1091
 source "$(dirname "${BASH_SOURCE[0]}")"/aaos_environment.sh "$0"
