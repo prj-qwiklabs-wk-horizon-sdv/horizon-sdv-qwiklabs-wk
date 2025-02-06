4cd80969258d460d162e469555a8e67ef5b1a329
marta-accenture <marta.kania@accenture.com>
2025-02-06 10:45:57 +0100

    Revert "[WILL BE REVERTED] Freeze keycloak-post-gerrit-job"
    This reverts commit dc7962d98d75e35978d36e4ebc820292b40b5f95.

--- gitops/env/dev/horizon-sdv-stage2/configs/keycloak/keycloak-post-gerrit/configure.sh
+++ gitops/env/dev/horizon-sdv-stage2/configs/keycloak/keycloak-post-gerrit/configure.sh
@@ -23,5 +23,3 @@ sed -i "s/##SSH_KEY_PUB##/${SSH_KEY_PUB}/g" ./secret.json
 
 curl --cacert ${CACERT} --header "Authorization: Bearer ${TOKEN}" -X DELETE ${APISERVER}/api/v1/namespaces/gerrit/secrets/gerrit-secure-config
 curl --cacert ${CACERT} --header "Authorization: Bearer ${TOKEN}" -H 'Accept: application/json' -H 'Content-Type: application/json' -X POST ${APISERVER}/api/v1/namespaces/gerrit/secrets -d @secret.json
-
-sleep 120000
